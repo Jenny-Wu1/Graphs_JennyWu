eccc3e62aa4f1c11196b098deccb61ec155cf29f
Jenny Wu <jennywu22553@gmail.com>
2025-04-21 17:31:01 -0500

    Implemented the checkType method that checks if a given matrix represents a directed or undirected graph

--- src/DirectedOrUndirected.java
+++ src/DirectedOrUndirected.java
@@ -1,2 +1,12 @@
 public class DirectedOrUndirected {
+    public static String checkType(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    return "The matrix represents a directed graph.";
+                }
+            }
+        }
+        return "The matrix represents an undirected graph.";
+    }
 }
